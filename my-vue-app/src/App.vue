e7f2f283e53056e15cbdff58f1af66c66ca1d9fb
marcelabetancur <mar.betancur3107@gmail.com>
2025-02-28 22:05:24 +0000

    feat: Se crea el menu y se actualiza el codigo fuente

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import MenuPrincipal from './components/Menu.vue'
+</script>
+
+<template>
+  <div>
+    <div class="container-fluid"></div>
+    <MenuPrincipal/>
+    <HelloWorld/>
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ my-vue-app/src/components/Menu.vue
@@ -0,0 +1,40 @@
+<!--Esta seccion de los templates sirve para todo el codigo html-->
+<template>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">nombre del proyecto</a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contactos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Productos</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+</template>
+
+
+<!--Esta seccion sirve para la logica y es neto js-->
+<script>
+
+
+</script>
+
+<!--Esta seccion se encarga del diseño-->
+<style>
+
+</style>
